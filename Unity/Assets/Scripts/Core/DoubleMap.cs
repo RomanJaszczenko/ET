eab38e7dd3517bc1fcd1540eb531671564a37ead
Roman Jaszczenko <88289708+RomanJaszczenko@users.noreply.github.com>
2023-12-13 23:59:44 +0100

    Update DoubleMap.cs
    Changes Made:
    Replaced explicit invocation of Keys with a foreach loop.
    Replaced null checks and ContainsKey with direct condition checks.
    Utilized tuples in ForEach for more concise code.
    Streamlined the methods GetValueByKey and GetKeyByValue using TryGetValue.

--- Unity/Assets/Scripts/Core/DoubleMap.cs
+++ Unity/Assets/Scripts/Core/DoubleMap.cs
@@ -3,142 +3,79 @@ using System.Collections.Generic;
 
 namespace ET
 {
-	public class DoubleMap<K, V>
-	{
-		private readonly Dictionary<K, V> kv = new();
-		private readonly Dictionary<V, K> vk = new();
+    public class DoubleMap<K, V>
+    {
+        private readonly Dictionary<K, V> kv = new();
+        private readonly Dictionary<V, K> vk = new();
 
-		public DoubleMap()
-		{
-		}
+        public DoubleMap()
+        {
+        }
 
-		public DoubleMap(int capacity)
-		{
-			kv = new Dictionary<K, V>(capacity);
-			vk = new Dictionary<V, K>(capacity);
-		}
+        public DoubleMap(int capacity)
+        {
+            kv = new Dictionary<K, V>(capacity);
+            vk = new Dictionary<V, K>(capacity);
+        }
 
-		public void ForEach(Action<K, V> action)
-		{
-			if (action == null)
-			{
-				return;
-			}
-			Dictionary<K, V>.KeyCollection keys = kv.Keys;
-			foreach (K key in keys)
-			{
-				action(key, kv[key]);
-			}
-		}
+        public void ForEach(Action<K, V> action)
+        {
+            foreach (var (key, value) in kv)
+            {
+                action?.Invoke(key, value);
+            }
+        }
 
-		public List<K> Keys
-		{
-			get
-			{
-				return new List<K>(kv.Keys);
-			}
-		}
+        public List<K> Keys => new List<K>(kv.Keys);
+        public List<V> Values => new List<V>(vk.Keys);
 
-		public List<V> Values
-		{
-			get
-			{
-				return new List<V>(vk.Keys);
-			}
-		}
+        public void Add(K key, V value)
+        {
+            if (key == null || value == null || kv.ContainsKey(key) || vk.ContainsKey(value))
+            {
+                return;
+            }
 
-		public void Add(K key, V value)
-		{
-			if (key == null || value == null || kv.ContainsKey(key) || vk.ContainsKey(value))
-			{
-				return;
-			}
-			kv.Add(key, value);
-			vk.Add(value, key);
-		}
+            kv.Add(key, value);
+            vk.Add(value, key);
+        }
 
-		public V GetValueByKey(K key)
-		{
-			if (key != null && kv.ContainsKey(key))
-			{
-				return kv[key];
-			}
-			return default(V);
-		}
+        public V GetValueByKey(K key) => key != null && kv.TryGetValue(key, out var value) ? value : default;
 
-		public K GetKeyByValue(V value)
-		{
-			if (value != null && vk.ContainsKey(value))
-			{
-				return vk[value];
-			}
-			return default(K);
-		}
+        public K GetKeyByValue(V value) => value != null && vk.TryGetValue(value, out var key) ? key : default;
 
-		public void RemoveByKey(K key)
-		{
-			if (key == null)
-			{
-				return;
-			}
-			V value;
-			if (!kv.TryGetValue(key, out value))
-			{
-				return;
-			}
+        public void RemoveByKey(K key)
+        {
+            if (key == null || !kv.TryGetValue(key, out var value))
+            {
+                return;
+            }
 
-			kv.Remove(key);
-			vk.Remove(value);
-		}
+            kv.Remove(key);
+            vk.Remove(value);
+        }
 
-		public void RemoveByValue(V value)
-		{
-			if (value == null)
-			{
-				return;
-			}
+        public void RemoveByValue(V value)
+        {
+            if (value == null || !vk.TryGetValue(value, out var key))
+            {
+                return;
+            }
 
-			K key;
-			if (!vk.TryGetValue(value, out key))
-			{
-				return;
-			}
+            kv.Remove(key);
+            vk.Remove(value);
+        }
 
-			kv.Remove(key);
-			vk.Remove(value);
-		}
+        public void Clear()
+        {
+            kv.Clear();
+            vk.Clear();
+        }
 
-		public void Clear()
-		{
-			kv.Clear();
-			vk.Clear();
-		}
+        public bool ContainsKey(K key) => key != null && kv.ContainsKey(key);
 
-		public bool ContainsKey(K key)
-		{
-			if (key == null)
-			{
-				return false;
-			}
-			return kv.ContainsKey(key);
-		}
+        public bool ContainsValue(V value) => value != null && vk.ContainsKey(value);
 
-		public bool ContainsValue(V value)
-		{
-			if (value == null)
-			{
-				return false;
-			}
-			return vk.ContainsKey(value);
-		}
-
-		public bool Contains(K key, V value)
-		{
-			if (key == null || value == null)
-			{
-				return false;
-			}
-			return kv.ContainsKey(key) && vk.ContainsKey(value);
-		}
-	}
+        public bool Contains(K key, V value) => key != null && value != null && kv.ContainsKey(key) && vk.ContainsKey(value);
+    }
 }
